ef855ee7ac6418a0821c3bf29983c10b0350240e
kmakai <makaikwag@gmail.com>
2023-10-18 17:29:37 -0400

    intial setup

---
+++ kmakai.sleeptracker.angular/Controllers/RecordController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using kmakai.sleeptracker.angular.Data;
+using kmakai.sleeptracker.angular.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace kmakai.sleeptracker.angular.Controllers;
+
+[Route("api/Sleep[controller]")]
+[ApiController]
+public class RecordController : ControllerBase
+{
+    private readonly SleepTrackerContext _context;
+    public RecordController(SleepTrackerContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Record>>> GetRecords()
+    {
+        return await _context.Records.ToListAsync();
+    }
+}
--- kmakai.sleeptracker.angular/Program.cs
+++ kmakai.sleeptracker.angular/Program.cs
@@ -1,6 +1,21 @@
+using kmakai.sleeptracker.angular.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<SleepTrackerContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SleepTrackerContext")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,5 +36,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowAll");
 
 app.Run();
